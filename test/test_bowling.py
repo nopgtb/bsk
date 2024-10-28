45be1a39e91579ae9f0d8bccf23c1806b7b801c7
NopGtb <npyyny19@student.oulu.fi>
2024-10-28 17:15:03 +0200

    US-2: Added empty game and full game score tests

--- test/test_bowling.py
+++ test/test_bowling.py
@@ -45,3 +45,21 @@ class TestBowlingGame(unittest.TestCase):
         game.add_frame(Frame(8, 1))
         game.add_frame(Frame(2, 6))
         self.assertRaises(BowlingError, game.add_frame, Frame(1, 5))
+
+    def test_score_empty_game(self):
+        game = BowlingGame()
+        self.assertEquals(0, game.calculate_score())
+
+    def test_score_10_frames(self):
+        game = BowlingGame()
+        game.add_frame(Frame(1, 5))
+        game.add_frame(Frame(3, 6))
+        game.add_frame(Frame(7, 2))
+        game.add_frame(Frame(3, 6))
+        game.add_frame(Frame(4, 4))
+        game.add_frame(Frame(5, 3))
+        game.add_frame(Frame(3, 3))
+        game.add_frame(Frame(4, 5))
+        game.add_frame(Frame(8, 1))
+        game.add_frame(Frame(2, 6))
+        self.assertEquals(81, game.calculate_score())
