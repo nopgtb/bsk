02ec3e730a912e78051e35c5b91218daeb5a34be
NopGtb <npyyny19@student.oulu.fi>
2024-10-28 17:08:56 +0200

    US-1: add 10 and 11 frames to the game tests

--- test/test_bowling.py
+++ test/test_bowling.py
@@ -16,3 +16,32 @@ class TestBowlingGame(unittest.TestCase):
     def test_raises_bowling_error(self):
         game = BowlingGame()
         self.assertRaises(BowlingError, game.get_frame_at, 0)
+
+    def test_add_10_frames(self):
+        game = BowlingGame()
+        game.add_frame(Frame(1, 5))
+        game.add_frame(Frame(3, 6))
+        game.add_frame(Frame(7, 2))
+        game.add_frame(Frame(3, 6))
+        game.add_frame(Frame(4, 4))
+        game.add_frame(Frame(5, 3))
+        game.add_frame(Frame(3, 3))
+        game.add_frame(Frame(4, 5))
+        game.add_frame(Frame(8, 1))
+        test_frame = Frame(2, 6)
+        game.add_frame(test_frame)
+        self.assertEquals(test_frame, game.get_frame_at(9))
+
+    def test_add_11_frames(self):
+        game = BowlingGame()
+        game.add_frame(Frame(1, 5))
+        game.add_frame(Frame(3, 6))
+        game.add_frame(Frame(7, 2))
+        game.add_frame(Frame(3, 6))
+        game.add_frame(Frame(4, 4))
+        game.add_frame(Frame(5, 3))
+        game.add_frame(Frame(3, 3))
+        game.add_frame(Frame(4, 5))
+        game.add_frame(Frame(8, 1))
+        game.add_frame(Frame(2, 6))
+        self.assertRaises(BowlingError, game.add_frame, Frame(1, 5))
